25c130cfcaabcca7efa3b5bd1d65f707d267c5f1
prathima-suryadevara <sree.smiley15@gmail.com>
2025-01-20 19:41:12 +0530

    StarPyramid

---
+++ Day2/StarPyramid.java
@@ -0,0 +1,19 @@
+package Day2;
+import java.util.Scanner;
+public class StarPyramid {
+public static void main(String[] args) {
+    System.out.println("Enter a number");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for (int i=1;i<=n;i++){
+        for (int j=i;j<n;j++){
+            System.out.print(" ");  
+        }
+        for (int k=1;k<=(2*i-1);k++) {
+            System.out.print("*"); 
+        }
+        System.out.println();
+        sc.close();
+}
+}
+}
