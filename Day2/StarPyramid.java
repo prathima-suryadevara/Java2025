a88b07698ffd77e88dfb1d0c52a8181897c17e31
prathima-suryadevara <sree.smiley15@gmail.com>
2025-01-21 19:07:19 +0530

    Making my first commit

--- Day2/StarPyramid.java
+++ Day2/StarPyramid.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
             System.out.print(" ");  
         }
         for (int k=1;k<=(2*i-1);k++) {
-            System.out.print("*"); 
+        System.out.print("*"); 
         }
         System.out.println();
         sc.close();
